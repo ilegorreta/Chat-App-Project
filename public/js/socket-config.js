f89757eb3033fcf65e58e6ffbbcd2bcbc52070b4
casiomachine <rodrigoffvh@gmail.com>
2020-11-14 00:06:48 -0600

    Separation of concerns via admin messages channel. Overall Refactoring.

--- public/js/socket-config.js
+++ public/js/socket-config.js
@@ -1,28 +1,44 @@
-var socket = io();
-var params = new URLSearchParams(window.location.search);
-var board = $('#Board');
-var roomTitle = $('h2');
+// Client-side logic for Chat Rooms
+
+// Agents enum
+const AGENT = {
+    PERSONAL: 'personal',
+    EXTERNAL: 'external',
+    ADMIN: 'admin'
+}
+
+// Events enum
+const EVENTS = {
+    LOGIN: 'login',
+    SEND: 'send',
+    MESSAGE: 'message',
+    INTERCOM: 'internal (admin) communication',
+}
+
+var socket = io()
+var params = new URLSearchParams(window.location.search)
+let board
 
 //On is to listen events
 socket.on('connect', function() {
-    console.log('Connected to the server (:');
-    socket.emit('login', {
+    console.log('Connected to the server (:')
+    socket.emit(EVENTS.LOGIN, {
         name: params.get('name'),
         room: params.get('room')
     })
-});
+})
 
-socket.on('message', function(data) {
-    renderMessage({
-        name: data.name,
-        message: data.message,
-        user: data.user
-    });
+socket.on(EVENTS.MESSAGE, function(data) {
+    renderMessage(data)
+})
+
+socket.on(EVENTS.INTERCOM, (data) => {
+    renderMessage(data)
 })
 
 socket.on('disconnect', function() {
-    console.log('Connection to the server is lost :(');
-});
+    console.log('Connection to the server is lost :(')
+})
 
 
 if (!localStorage.id) {
@@ -33,6 +49,10 @@ let id = localStorage.id
 
 $(function() {
 
+    // Not in use
+    board = $('#Board')
+    
+    let roomTitle = $('h2')
     roomTitle.html(`Room: ${params.get('room')}`)
 
     $('#messageForm').submit(function(e) {
@@ -42,18 +62,18 @@ $(function() {
         // This is the text message from the form submission
         let message = $('#inputField').val()
 
-        socket.emit('send', {
+        socket.emit(EVENTS.SEND, {
             id: id,
             name: params.get('name'),
             room: params.get('room'),
             message: message
-        });
+        })
 
         renderMessage({
             name: params.get('name'),
             message: message,
-            user: "me"
-        });
+            user: AGENT.PERSONAL
+        })
 
         // Clear the text field with an empty string
         $('#inputField').val('')
@@ -62,6 +82,22 @@ $(function() {
 })
 
 function renderMessage(message) {
+    let html = ''
+
+    if (message.user == AGENT.ADMIN) {
+        console.log(AGENT.ADMIN)
+        html = `<li class="messagesAdmin"><h5>${message.name}: ${message.message}</h5></li>`
+    } else if (message.user == AGENT.EXTERNAL) {
+        html = `<li class="messagesOthers"><h5>${message.name}: ${message.message}</h5></li>`
+
+    } else if (message.user == AGENT.PERSONAL) {
+        html = `<li class="messagesMe"><h5>${message.name}: ${message.message}</h5></li>`
+    }
+
+    board.append(html)
+}
+
+    /*
     if (message.user === "me") {
         var html = `<li class="messagesMe"><h5> ${message.name}: ${message.message}</h5></li>`;
     } else if (message.user === "admin") {
@@ -69,9 +105,4 @@ function renderMessage(message) {
     } else if (message.user === "other") {
         var html = `<li class="messagesOthers"><h5> ${message.name}: ${message.message}</h5></li>`;
     }
-    board.append(html);
-}
-
-module.exports = {
-    renderMessage
-}
+    */
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,34 +1,35 @@
-const { io } = require('../server');
-const { User } = require('../classes/user');
+const { io } = require('../server')
+const { User } = require('../classes/user')
+const { AGENT, EVENTS } = require('../../public/js/utils.js')
 const users = new User();
 
 io.on('connection', (client) => {
 
-    client.on('login', (data) => {
+    client.on(EVENTS.LOGIN, (data) => {
         let user = users.addUser(client.id, data.name, data.room)
         client.join(data.room)
-        client.broadcast.to(data.room).emit('message', {
+        client.broadcast.to(data.room).emit(EVENTS.INTERCOM, {
             name: data.name,
-            message: `${data.name} join the room`,
-            user: "admin"
+            message: `${data.name} joined the room`,
+            user: AGENT.ADMIN
         });
     })
 
-    client.on('send', (data) => {
-        client.broadcast.to(data.room).emit('message', {
+    client.on(EVENTS.SEND, (data) => {
+        client.broadcast.to(data.room).emit(EVENTS.MESSAGE, {
             name: data.name,
             message: data.message,
-            user: "other"
+            user: AGENT.EXTERNAL
         });
     })
 
     client.on('disconnect', () => {
         console.log('User disconnected');
-        user = users.getUser(client.id);
-        client.broadcast.to(user.room).emit('message', {
+        let user = users.getUser(client.id);
+        client.broadcast.to(user.room).emit(EVENTS.INTERCOM, {
             name: user.name,
             message: `${user.name} left the room`,
-            user: "admin"
+            user: AGENT.ADMIN
         });
     });
 });
